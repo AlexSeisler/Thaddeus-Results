65d0d36e1a7f5f1475d8a391cee3857bd39b62fc
Alex Seisler <aseisler7045@stevenscollege.edu>
2024-11-04 17:53:00 -0500

    phone number

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function closePopup()
 function initiateCall() {
     if (/Mobi|Android/i.test(navigator.userAgent)) {
         // Mobile device, use the tel link
-        window.location.href = "tel:+4847694650";
+        window.location.href = "tel:+14847694650";
     } else {
         // Desktop device, redirect to desktop app or call app URL
         window.location.href = "https://teams.microsoft.com/l/meetup-join/273183261025";
