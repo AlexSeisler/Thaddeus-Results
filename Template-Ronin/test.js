895ace8cf49eb7e1d1b48b1682c14b89c5ba3b71
TheLonelySamurai <yayasanouvong@gmail.com>
2024-11-05 13:59:12 -0500

    header anims media queries fix template ronin

---
+++ Template-Ronin/test.js
@@ -0,0 +1,6 @@
+// let skills = document.body.getElementsByClassName("skill");
+// for (let i = 0; i < skills.length; i++){
+//     console.log(skills[i].innerHTML);
+//     skills[i].innerHTML += '!';
+//     console.log(skills[i].innerHTML);
+// }
