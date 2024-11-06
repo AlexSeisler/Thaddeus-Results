1bbb7ca5bf0f689513deaf55ee7fa4a97c033cb9
Alex Seisler <aseisler7045@stevenscollege.edu>
2024-11-06 10:12:26 -0500

    popup and group links

--- script.js
+++ script.js
@@ -29,15 +29,15 @@ document.addEventListener('DOMContentLoaded', function() {
     showPopup();
 
 });
-var count = 0
+var count = 0;
 function closePopup() 
 {
     
     if (/Mobi|Android/i.test(navigator.userAgent) && count == 0) {
         // Mobile device
         document.getElementById('youtube-video').style.display = "none";
-        document.getElementById('microsoft-form').style.height = "100vh";
-        document.getElementById('microsoft-form').style.width = "640px";
+        document.getElementById('microsoft-form').style.height = "90vh";
+        document.getElementById('microsoft-form').style.width = "100vw";
         count++;
     }
     else
@@ -55,3 +55,4 @@ function initiateCall() {
         window.location.href = "https://teams.microsoft.com/l/meetup-join/273183261025";
     }
 }
+
